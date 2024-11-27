cda1745eb73e78e51e0bce289159754cf87df759
Hejhejtacktack <114874962+Hejhejtacktack@users.noreply.github.com>
2024-11-27 12:35:11 +0100

    rename args

--- make-call.py
+++ make-call.py
@@ -10,9 +10,9 @@ parser = argparse.ArgumentParser(description="Make a call and forward it to {who
 
 parser.add_argument("API_USERNAME", type=str, help="Your 46elks API username")
 parser.add_argument("API_PASSWORD", type=str, help="Your 46elks API password")
-parser.add_argument("Caller", type=str, help="Caller's phone number in E.164 format (e.g.+46701234567)."
+parser.add_argument("caller", type=str, help="Caller's phone number in E.164 format (e.g.+46701234567)."
                                              "This number shows as caller (also when forwarding call)")
-parser.add_argument("Callee", type=str, help="Callee's phone number in E.164 format (e.g.+46701234567)")
+parser.add_argument("callee", type=str, help="Callee's phone number in E.164 format (e.g.+46701234567)")
 
 args = parser.parse_args()
 
@@ -26,8 +26,8 @@ action = {
 }
 
 fields = {
-    'from': args.Caller,
-    'to': args.Callee,
+    'from': args.caller,
+    'to': args.callee,
     'voice_start': json.dumps(action),
     }
 
